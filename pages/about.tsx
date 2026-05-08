9caab331965ee1e1c7fd7e2d18350f7a0d97698d
muhammadtoor702-blip <muhammadtoor702@gmail.com>
2026-05-09 01:39:14 +0500

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -26,11 +26,11 @@ export default function About(): JSX.Element {
             </p>
           </div>
 
-          <div className="w-full overflow-hidden rounded-[20px] border border-[#e8e4dc] bg-[#f7f7f7] sm:rounded-[24px] md:rounded-[28px] max-h-[400px] md:max-h-[520px] md:h-[520px]">
+          <div style={{ width: '100%', borderRadius: '20px', border: '1px solid #e8e4dc', background: '#f7f7f7', overflow: 'hidden' }}>
   <img
     src="/wahab.jpg"
     alt="Muhammad Abdul Wahab Toor"
-    className="w-full h-full object-cover object-contain"
+    style={{ width: '100%', height: 'auto', display: 'block' }}
   />
 </div>
         </div>
